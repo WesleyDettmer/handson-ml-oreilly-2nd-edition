e2f8021658203a13405a8edc0bb3d0f8f493b7f5
WesleyDettmer <w.dettmer1996@gmail.com>
2022-08-05 21:42:19 -0300

    Primeiro Código - Lembrar de colocar as fontes

--- Part 1 - The Fundamentals of Machine Learning/Example1-1.py
+++ Part 1 - The Fundamentals of Machine Learning/Example1-1.py
@@ -3,3 +3,29 @@ import numpy as np
 import pandas as pd
 import sklearn.linear_model
 
+# Load data / Carregar dados
+
+oecd_bli = pd.read_csv("oecd_bli_2015.csv", thousands=',')
+gdp_per_carpita = pd.read_csv("gdp_per_capita.csv", thousands=',' ,delimiter='\t', encoding='latin1', na_values="n/a")
+
+# Prepare data / preparar dados
+
+country_stats = prepare_country_stats(oecd_bli, gdp_per_carpita)
+x = np.c_[country_stats["GPD per capita"]]
+y = np.c_[country_stats["Life satisfaction"]]
+
+# Visualize the data / Visualizando os dados
+
+country_stats.plot(kind='scatter', x='GPD per capita', y='Life satisfaction')
+plt.show()
+
+# Select linear model
+model = sklearn.linear_model.LinearRegression()
+
+# Train the model
+model.fit(x, y)
+
+# Make a prediction for Cyprus
+
+X_new = [[22587]]
+print(model.predict(X_new))
